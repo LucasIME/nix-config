7ff185dc906ab0f75180e40ff00192b87e64c13d
Lucas Sousa Meireles <meireles31@msn.com>
2026-07-31 08:04:52 +0100

    modern names

--- configuration.nix
+++ configuration.nix
@@ -86,8 +86,8 @@ in
   };
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -139,6 +139,7 @@ in
   services.tailscale = {
     enable = true;
   };
+
 
   programs.dconf.profiles.user.databases = [
     {
